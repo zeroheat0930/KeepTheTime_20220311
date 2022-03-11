8fa502b54036ee72ebbc361f86fb6a379e8c8e98
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-11 16:12:43 +0900

    로그인 함수 호출 > 서버 기능 Call  - 임시 Callback 등록 : 응답 내용만 확인

--- app/src/main/java/com/zeroheat/keepthetime_20220311/SignInActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/SignInActivity.kt
@@ -1,8 +1,13 @@
 package com.zeroheat.keepthetime_20220311
 
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.zeroheat.keepthetime_20220311.databinding.ActivitySignInBinding
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignInActivity : BaseActivity() {
 
@@ -16,6 +21,23 @@ class SignInActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.btnLogin.setOnClickListener {
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPassword = binding.edtPassword.text.toString()
+
+            apiList.postRequestLogin(inputEmail, inputPassword).enqueue(object : Callback<JSONObject>{
+                override fun onResponse(call: Call<JSONObject>, response: Response<JSONObject>) {
+
+                    Log.d("응답확인", response.toString())
+                }
+
+                override fun onFailure(call: Call<JSONObject>, t: Throwable) {
+//                    서버에 물리적 연결 실패
+                }
+
+            })
+        }
 
     }
 
