1c199ec2fe4e8157a29634a2a2c4101056654eff
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-11 16:02:06 +0900

    JSON 파싱을 쉽게 도와주는 GSON 라이브러리 결합

--- app/src/main/java/com/zeroheat/keepthetime_20220311/api/ServerAPI.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/api/ServerAPI.kt
@@ -1,11 +1,13 @@
 package com.zeroheat.keepthetime_20220311.api
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ServerAPI {
 
 //    Retrofit 클래스 객체화 함수 => 단일 객체만 만들어서, 모든 화면에서 공유.
 //     여러개의 객체를 만들 필요 X. SingleTon 패턴
+
 
     companion object {
 
@@ -19,11 +21,13 @@ class ServerAPI {
         fun getRetrofit() : Retrofit {
 
 //           Retrofit 라이브러리는, 클래스 차원에서 BASE_URL을 설정할 수 있게 도와줌.
+//           Retrofit + Gson 두개의 라이브러리를 결합하면 => JSON 파싱이 쉬워짐.
 
 
             if (retrofit == null) {
                 retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL) // 어느 서버를 기반으로 움직일건지.
+                    .addConverterFactory( GsonConverterFactory.create() ) // gson 라이브러리와 결합
                     .build()
             }
 
