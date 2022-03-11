853c6566a13577f965e88b5dea37a4dffe955942
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-11 16:05:19 +0900

    Retrofit 클래스 객체 / APIList 인터페이스 결합 => apiList 인터페이스 변수 상속 처리.

--- app/src/main/java/com/zeroheat/keepthetime_20220311/BaseActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/BaseActivity.kt
@@ -3,14 +3,23 @@ package com.zeroheat.keepthetime_20220311
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.zeroheat.keepthetime_20220311.api.APIList
+import com.zeroheat.keepthetime_20220311.api.ServerAPI
 
 abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var mContext: Context
 
+//    모든 화면에서, apiList 변수가 있다면 => apiList.서버기능( ) 형태로 간단히 코딩 가능.
+    lateinit var apiList: APIList
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = this
+
+        val retrofit = ServerAPI.getRetrofit()
+        apiList = retrofit.create( APIList::class.java )
     }
 
     abstract fun setupEvents()
