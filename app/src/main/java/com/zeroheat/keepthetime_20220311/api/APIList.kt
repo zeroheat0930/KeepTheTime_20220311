9aa3aa4d18984706116166d6881f9b2cf8b50bb6
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-11 16:03:09 +0900

    인터페이스에, 로그인 기능을 위한 함수 명세 작성.

--- app/src/main/java/com/zeroheat/keepthetime_20220311/api/APIList.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/api/APIList.kt
@@ -1,4 +1,19 @@
 package com.zeroheat.keepthetime_20220311.api
 
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface APIList {
+//    BASE_URL에 해당하는 서버에서, 어떤 기능들을 사용할건지 명시.
+
+    @FormUrlEncoded // 파라미터중에, Field (formData)에 담아야하는 파라미터가 있다면 필요한 구문.
+    @POST("/user")
+    fun postRequestLogin(
+        @Field("email") email: String,
+        @Field("password") pw: String
+    ) : Call<JSONObject>
+
 }
