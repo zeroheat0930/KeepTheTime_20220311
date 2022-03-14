3afc6aeff56f44919bd39b740a4a6fdfce38243d
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-14 09:55:23 +0900

    메인화면 연습문제 준비  - 내 정보 조회 API (GET) 활용  - 토큰값 필요 -> ContextUtil 작업

--- app/src/main/java/com/zeroheat/keepthetime_20220311/SignInActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/SignInActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.zeroheat.keepthetime_20220311.databinding.ActivitySignInBinding
 import com.zeroheat.keepthetime_20220311.datas.BasicResponse
+import com.zeroheat.keepthetime_20220311.utils.ContextUtil
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -55,6 +56,21 @@ class SignInActivity : BaseActivity() {
 //                        Retrofit의 Callback은 UIThread 안으로 다시 돌아오도록 처리되어있다.
 //                        UI 조작을 위해 runOnUiThread {  } 작성 필요 X.
                         Toast.makeText(mContext, "${br.data.user.nick_name}님, 환영합니다!", Toast.LENGTH_SHORT).show()
+
+
+//                        서버가 내려주는 토큰값을 저장.
+                        ContextUtil.setLoginUserToken(mContext,  br.data.token)
+
+//                        메인화면으로 이동, 현재 화면 종료
+
+                        val myIntent = Intent(mContext, MainActivity::class.java)
+                        startActivity(myIntent)
+
+                        finish()
+
+
+
+
 
 
                     }
---
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/utils/ContextUtil.kt
@@ -0,0 +1,25 @@
+package com.zeroheat.keepthetime_20220311.utils
+
+import android.content.Context
+
+class ContextUtil {
+
+    companion object {
+
+        private val prefName = "KeepTheTimePref"
+
+        private val LOGIN_USER_TOKEN = "LOGIN_USER_TOKEN"
+
+        fun setLoginUserToken(context: Context, token: String ) {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putString(LOGIN_USER_TOKEN,  token).apply()
+        }
+
+        fun getLoginUserToken( context: Context ) : String {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getString(LOGIN_USER_TOKEN, "")!!
+        }
+
+    }
+
+}
