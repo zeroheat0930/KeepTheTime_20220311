9628527ba26cd3a9059a6e9cd4f5bd03415eec89
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-14 16:27:22 +0900

    리싸이클러뷰의 어댑터 제작 중  - RecyclerView.Adpater 상속  - inner class로 MyViewHolder 생성  - 어댑터가 다룰 항목으로 MyViewHolder 선정

---
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/adapters/SearchedUserRecyclerAdapter.kt
@@ -0,0 +1,25 @@
+package com.zeroheat.keepthetime_20220311.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchedUserRecyclerAdapter : RecyclerView.Adapter<SearchedUserRecyclerAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
