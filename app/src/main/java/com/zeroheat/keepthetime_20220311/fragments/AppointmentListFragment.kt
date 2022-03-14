fc2af148f8b6972e56deed52e6094afb2768f5b2
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-14 11:44:38 +0900

    연습문제 - 약속목록프래그먼트에 BaseFragment, 데이터바인딩 세팅 적용

--- app/src/main/java/com/zeroheat/keepthetime_20220311/fragments/AppointmentListFragment.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/fragments/AppointmentListFragment.kt
@@ -4,22 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.zeroheat.keepthetime_20220311.R
+import com.zeroheat.keepthetime_20220311.databinding.FragmentAppointmentListBinding
 
-class AppointmentListFragment : Fragment() {
+class AppointmentListFragment : BaseFragment() {
+
+    lateinit var binding: FragmentAppointmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_appointment_list, container, false)
+        binding = DataBindingUtil.inflate( inflater,  R.layout.fragment_appointment_list, container, false  )
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
 
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+        binding.txtTest.text = "코틀린에서 문구 바꾸기"
 
     }
 
