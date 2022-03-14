f5d60e9832c12190eda0d20c2d8482bea7f23758
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-14 11:55:36 +0900

    자동로그인 기능 구현 완료  - 임시 : 내 정보 조회 성공 여부만 체크.

--- app/src/main/java/com/zeroheat/keepthetime_20220311/SplashActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/SplashActivity.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.zeroheat.keepthetime_20220311.datas.BasicResponse
+import com.zeroheat.keepthetime_20220311.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +24,40 @@ class SplashActivity : BaseActivity() {
 
     override fun setValues() {
 
-//        임시 - 자동 로그인 구현 전까지는, 2.5초 후에 무조건 로그인화면으로.
+//        실제 - 저장된 토큰으로, 내 정보 조회 시도 먼저 진행.
+//          2.5초 후에, 내 정보가 불러와졌는지? 결과에 따라 다른 화면으로 이동.
+
+        var isMyInfoLoaded = false
+
+        apiList.getRequestMyInfo( ContextUtil.getLoginUserToken(mContext) ).enqueue(object :
+            Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+//                CODE : 200 => 성공 응답이 왔다? 내 정보가 일단 잘 불러졌다.
+                if (response.isSuccessful) {
+                    isMyInfoLoaded = true
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
 
         val myHandler = Handler(Looper.getMainLooper())
 
         myHandler.postDelayed( {
 
-            val myIntent = Intent(mContext, SignInActivity::class.java)
-            startActivity(myIntent)
+            val myIntent : Intent
 
-            finish()
+            if (isMyInfoLoaded) {
+                myIntent = Intent(mContext, MainActivity::class.java)
+            }
+            else {
+                myIntent = Intent(mContext, SignInActivity::class.java)
+            }
+
+            startActivity(myIntent)
 
         } , 2500)
 
