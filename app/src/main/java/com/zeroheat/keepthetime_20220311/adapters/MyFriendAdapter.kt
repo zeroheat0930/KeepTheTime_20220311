309420f8c3f9aaa227bf143ab629a48625f7be1a
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-14 16:07:35 +0900

    리스트뷰 어댑터 뼈대작업 마무리

--- app/src/main/java/com/zeroheat/keepthetime_20220311/adapters/MyFriendAdapter.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/adapters/MyFriendAdapter.kt
@@ -1,7 +1,11 @@
 package com.zeroheat.keepthetime_20220311.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.zeroheat.keepthetime_20220311.R
 import com.zeroheat.keepthetime_20220311.datas.UserData
 
 class MyFriendAdapter(
@@ -9,6 +13,19 @@ class MyFriendAdapter(
     resId: Int,
     val mList: List<UserData>
 ) : ArrayAdapter<UserData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow =  LayoutInflater.from(mContext).inflate(R.layout.my_friend_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+    }
 
 
 
