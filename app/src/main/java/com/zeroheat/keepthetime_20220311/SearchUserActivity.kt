5eb94b825372ab2d84cde52fede5ae528eb6098f
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-14 17:19:00 +0900

    리싸이클러 어댑터를 객체화, 리싸이클러뷰와 연결  - LayoutManager를 추가로 설정해야함.

--- app/src/main/java/com/zeroheat/keepthetime_20220311/SearchUserActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/SearchUserActivity.kt
@@ -3,6 +3,8 @@ package com.zeroheat.keepthetime_20220311
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.zeroheat.keepthetime_20220311.adapters.SearchedUserRecyclerAdapter
 import com.zeroheat.keepthetime_20220311.databinding.ActivitySearchUserBinding
 import com.zeroheat.keepthetime_20220311.datas.BasicResponse
 import com.zeroheat.keepthetime_20220311.datas.UserData
@@ -16,6 +18,8 @@ class SearchUserActivity : BaseActivity() {
     lateinit var binding: ActivitySearchUserBinding
 
     val mSearchedUserList = ArrayList<UserData>()
+
+    lateinit var mAdapter: SearchedUserRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,12 +42,17 @@ class SearchUserActivity : BaseActivity() {
                     call: Call<BasicResponse>,
                     response: Response<BasicResponse>
                 ) {
+//                    기존의 검색 목록은 삭제 해야, 누적으로 추가되는것을 막을 수 있다.
+
+                    mSearchedUserList.clear()
 
                     if (response.isSuccessful) {
 
                         val br = response.body()!!
 
                         mSearchedUserList.addAll( br.data.users )
+
+                        mAdapter.notifyDataSetChanged()
 
                     }
 
@@ -61,6 +70,12 @@ class SearchUserActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mAdapter = SearchedUserRecyclerAdapter(mContext, mSearchedUserList)
+        binding.userListRecyclerView.adapter = mAdapter
+
+//        리싸이클러뷰는, 어떤 모양으로 목록을 표현할지도 설정해야 화면에 데이터가 나옴.
+        binding.userListRecyclerView.layoutManager = LinearLayoutManager(mContext)
 
     }
 }
