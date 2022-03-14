ab6b55fe826b5589d6a774a2439c15c8fd7e5d33
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-14 11:08:57 +0900

    내 프로필 프래그먼트를, BaseFragment 상속  - 내 정보 조회 API를 쉽게 사용 가능.

--- app/src/main/java/com/zeroheat/keepthetime_20220311/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/fragments/MyProfileFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.zeroheat.keepthetime_20220311.R
+import com.zeroheat.keepthetime_20220311.datas.BasicResponse
+import com.zeroheat.keepthetime_20220311.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MyProfileFragment : Fragment() {
+class MyProfileFragment : BaseFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,8 +23,27 @@ class MyProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
 
+    override fun setupEvents() {
 
     }
 
+    override fun setValues() {
+
+//        내 정보 조회 > UI 반영
+        apiList.getRequestMyInfo(ContextUtil.getLoginUserToken(mContext)).enqueue(object :
+            Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+    }
 }
