d8ac458ea1279a362337dc78d96b5056795021e5
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-15 10:20:58 +0900

    친구 요청 목록 추가

---
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/fragments/RequestedUsersFragment.kt
@@ -0,0 +1,37 @@
+package com.zeroheat.keepthetime_20220311.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.zeroheat.keepthetime_20220311.R
+import com.zeroheat.keepthetime_20220311.databinding.FragmentRequestedUsersBinding
+
+class RequestedUsersFragment : BaseFragment() {
+
+    lateinit var binding: FragmentRequestedUsersBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_requested_users,container,false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
