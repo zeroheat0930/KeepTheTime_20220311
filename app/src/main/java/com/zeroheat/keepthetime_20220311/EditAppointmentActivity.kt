b7916cfb3836bc307ce86a90efe4b41e13b561e8
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-15 15:40:31 +0900

    날짜 선택시 - Calendar / SimpleDateFormat 으로 월 값 보정.

--- app/src/main/java/com/zeroheat/keepthetime_20220311/EditAppointmentActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/EditAppointmentActivity.kt
@@ -6,10 +6,16 @@ import android.widget.DatePicker
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.zeroheat.keepthetime_20220311.databinding.ActivityEditAppointmentBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditAppointmentActivity : BaseActivity() {
 
     lateinit var binding: ActivityEditAppointmentBinding
+
+//    약속 시간 일/시 를 저장해줄 Calendar. (월 값이 0~11로 움직이게 맞춰져있다.)
+    val mSelectedAppointmentDateTime = Calendar.getInstance() // 기본값 : 현재 일시
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +32,17 @@ class EditAppointmentActivity : BaseActivity() {
 
                 override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
 
-                    Toast.makeText(mContext, "${year}년 ${month}월 ${dayOfMonth}일 선택함.", Toast.LENGTH_SHORT).show()
+//                    연/월/일은, JAVA / Kotlin 언어의 기준 (0~11월)으로 월 값을 줌. (사람은 1~12월)
+//                    주는 그대로 Calendar에 set 하게되면, 올바른 월로 세팅됨.
+
+                    mSelectedAppointmentDateTime.set( year, month, dayOfMonth ) // 연월일 한번에 세팅 함수.
+
+//                    약속일자의 문구를 22/03/08 등의 형식으로 바꿔서 보여주자.
+//                    SimpleDateFormat을 활용 => 월값도 알아서 보정.
+
+                    val sdf = SimpleDateFormat("yy/MM/dd")
+
+                    binding.txtDate.text =  sdf.format(mSelectedAppointmentDateTime.time)
 
                 }
 
