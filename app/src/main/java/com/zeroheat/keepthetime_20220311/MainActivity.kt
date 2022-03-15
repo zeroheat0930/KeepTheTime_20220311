172bd881785bfd7f727efbf88aaf51d92e2aadf7
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-15 14:42:07 +0900

    네비게이션의 메뉴가 선택되면, 뷰 페이져의 페이지를 이동시키기

--- app/src/main/java/com/zeroheat/keepthetime_20220311/MainActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/MainActivity.kt
@@ -20,11 +20,29 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+//        바텀 네비게이션의 메뉴 선택 > 뷰페이져의 페이지 이동
+
+        binding.mainBottomNav.setOnItemSelectedListener {
+
+//            어떤 메뉴가 선택되었는지? it 변수가 알려줌.
+
+            when (it.itemId) {
+                R.id.myAppointment -> binding.mainViewPager2.currentItem = 0
+                R.id.myProfile -> binding.mainViewPager2.currentItem = 1
+            }
+
+            return@setOnItemSelectedListener true
+        }
+
+//        뷰페이저의 페이지 이동 > 바텀 네비게이션의 메뉴 선택
+
+
     }
 
     override fun setValues() {
 
         binding.mainViewPager2.adapter = MainViewPager2Adapter(this) // 변수 : Activity => 객체 : context로 대입 불가
+
 
 
     }
