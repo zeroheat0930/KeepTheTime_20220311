7a9e18dae2e6c277bafb908a1258f558e238971f
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-15 10:56:34 +0900

    내 친구 목록 - 리싸이클러뷰로 표시

--- app/src/main/java/com/zeroheat/keepthetime_20220311/adapters/MyFriendRecyclerAdapter.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/adapters/MyFriendRecyclerAdapter.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.zeroheat.keepthetime_20220311.R
 import com.zeroheat.keepthetime_20220311.datas.UserData
+import org.w3c.dom.Text
 
 class MyFriendRecyclerAdapter(
     val mContext : Context,
@@ -15,6 +19,49 @@ class MyFriendRecyclerAdapter(
 
     inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
 
+        val imgProfile = view.findViewById<ImageView>(R.id.imgProfile)
+        val txtNickname = view.findViewById<TextView>(R.id.txtNickname)
+        val imgSocialLoginLogo = view.findViewById<ImageView>(R.id.imgSocialLoginLogo)
+        val txtEmail = view.findViewById<TextView>(R.id.txtEmail)
+
+        fun bind(data: UserData){
+
+            Glide.with(mContext).load(data.profile_img).into(imgProfile)
+            txtNickname.text = data.nick_name
+//        사용자의 provider - "default" : 이메일 표시, "kakao" : 카카오 로그인, "facebook" : 페북 로그인, "naver" : 네이버 로그인
+//        txtEmail 자리에 반영
+            when (data.provider) {
+                "default" -> {
+//                이메일 표시
+                    txtEmail.text = data.email
+//                로고 이미지 숨김
+                    imgSocialLoginLogo.visibility = View.GONE
+                }
+                "kakao" -> {
+//                "카카오로그인"
+                    txtEmail.text = "카카오로그인"
+//                로고 이미지 : 카카오 아이콘
+                    imgSocialLoginLogo.visibility = View.VISIBLE
+                    Glide.with(mContext).load(R.drawable.kakao_logo).into(imgSocialLoginLogo)
+                }
+                "facebook" -> {
+                    txtEmail.text = "페북 로그인"
+                    imgSocialLoginLogo.visibility = View.VISIBLE
+                    Glide.with(mContext).load(R.drawable.facebook_logo).into(imgSocialLoginLogo)
+                }
+                "naver" -> {
+                    txtEmail.text  = "네이버 로그인"
+                    imgSocialLoginLogo.visibility = View.VISIBLE
+
+//                Glide는 웹의 이미지 뿐 아니라, 우리 프로젝트 내부의 이미지도 불러낼 수 있다.
+                    Glide.with(mContext).load(R.drawable.naver_logo).into(imgSocialLoginLogo)
+                }
+                else -> {
+//                그 외의 잘못된 경우.
+                }
+            }
+
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -25,6 +72,8 @@ class MyFriendRecyclerAdapter(
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
+        val data = mList[position]
+        holder.bind(data)
     }
 
     override fun getItemCount() = mList.size
