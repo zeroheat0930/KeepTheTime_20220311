3c00c041e0fc80b9bc83b141a0316b1e67907cb3
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-15 18:14:03 +0900

    22-03-15 수정완!

--- app/src/main/java/com/zeroheat/keepthetime_20220311/ManageMyFirendActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/ManageMyFirendActivity.kt
@@ -4,17 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.zeroheat.keepthetime_20220311.adapters.FriendViewPagerAdapter
-import com.zeroheat.keepthetime_20220311.databinding.ActivityManageMyFriendBinding
+import com.zeroheat.keepthetime_20220311.databinding.ActivityManageMyFirendBinding
 
 class ManageMyFriendsActivity : BaseActivity() {
 
-    lateinit var binding: ActivityManageMyFriendBinding
+    lateinit var binding: ActivityManageMyFirendBinding
 
     lateinit var mAdapter: FriendViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_my_friend)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_my_firend)
         setupEvents()
         setValues()
     }
