3cf632bfcca5258df3b899afc622f2d26c32862f
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-15 10:48:11 +0900

    서버에서 내 친구 목록 가져와서, ArrayList에 담아두기

--- app/src/main/java/com/zeroheat/keepthetime_20220311/fragments/MyFriendsFragment.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/fragments/MyFriendsFragment.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.zeroheat.keepthetime_20220311.R
 import com.zeroheat.keepthetime_20220311.databinding.FragmentMyFriendsBinding
+import com.zeroheat.keepthetime_20220311.datas.BasicResponse
+import com.zeroheat.keepthetime_20220311.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyFriendsFragment : BaseFragment() {
 
     lateinit var binding: FragmentMyFriendsBinding
+
+    val mMyFriendsList = ArrayList<UserData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,6 +40,29 @@ class MyFriendsFragment : BaseFragment() {
     }
 
     override fun setValues() {
+
+        getMyFriendsFromServer()
+    }
+
+    fun getMyFriendsFromServer(){
+
+        apiList.getRequestFriendList("my").enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful){
+                    val br = response.body()!!
+
+                    mMyFriendsList.clear()
+
+                    mMyFriendsList.addAll(br.data.friends)
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+        })
 
     }
 }
