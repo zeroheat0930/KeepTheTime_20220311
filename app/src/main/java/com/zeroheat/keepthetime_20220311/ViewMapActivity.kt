e3fb4a3697995390441497ac0b0893650e46c430
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-16 16:59:17 +0900

    대중교통 길찾기 추가 파싱 > 이동시간 / 비용 추출

--- app/src/main/java/com/zeroheat/keepthetime_20220311/ViewMapActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/ViewMapActivity.kt
@@ -54,13 +54,17 @@ class ViewMapActivity : BaseActivity() {
             marker.position = destLatLng
             marker.map = naverMap
 
-//            Path 객체의 좌표들 설정 => naverMap에 추가.
-
-            val path = PathOverlay()
-
 //            path.coords => 출발지 / 도착지만 넣어서 대입하면? 일직선 연결
 
 //            출발지~ 도착지 사이의 정거장이 있다면 정거장들을 좌표로 추가
+
+            val stationList = ArrayList<LatLng>()
+
+//            첫 좌표는 출발장소.
+
+            stationList.add( LatLng( mAppointment.start_latitude, mAppointment.start_longitude))
+
+//            거치게 되는 정거장들 목록을 > ODSayService로 받아서 주기.
 
             val myODsayService = ODsayService.init(mContext, "jI4STxcHo/C9hrHGxIjPqk9a3v6BZe2OSeT8r5LSzTI")
 
@@ -82,6 +86,59 @@ class ViewMapActivity : BaseActivity() {
 
                         val resultObj = jsonObj.getJSONObject("result")
                         Log.d("result확인", resultObj.toString())
+
+                        val pathArr = resultObj.getJSONArray("path") // 여러개의 추천 경로 => 0번째 경로로 사용. (코딩 편의)
+
+                        val firstPathObj = pathArr.getJSONObject(0)
+
+//                        첫 경로의 subPath 목록 파싱 (도보 - 버스 - 지하철 - 도보 ...)
+                        val subPathArr = firstPathObj.getJSONArray("subPath")
+
+                        for (i  in  0 until  subPathArr.length()) {
+
+                            val subPathObj = subPathArr.getJSONObject(i)
+
+                            if (!subPathObj.isNull("passStopList")) {
+//                                도보가 아니어서, 정거장 목록을 주는 경우.
+
+                                val passStopListObj = subPathObj.getJSONObject("passStopList")
+                                val stationsArr = passStopListObj.getJSONArray("stations")
+
+                                for (j in  0 until  stationsArr.length() ) {
+
+                                    val stationObj = stationsArr.getJSONObject(j)
+
+                                    val stationLat = stationObj.getString("y").toDouble()
+                                    val stationLng = stationObj.getString("x").toDouble()
+
+//                                    네이버 지도의 경로선에 그려줄 좌표 목록에 추가.
+                                    stationList.add( LatLng( stationLat, stationLng ) )
+
+                                }
+
+                            }
+
+                        }
+
+//                        모든 정거장 목록이 추가되어있다.
+//                        마지막 경로선으로, 도착지를 추가.
+
+                        stationList.add( destLatLng )
+
+
+//                        경로선을 지도에 그려주자.
+                        val path = PathOverlay()
+                        path.coords = stationList
+                        path.map = naverMap
+
+//                        파싱을 추가로 하면, 소요시간/ 비용 정보 얻을수 있다. =>  infoWindow에 결합.
+                        val infoObj = firstPathObj.getJSONObject("info")
+
+                        val minutes = infoObj.getInt("totalTime")
+                        val payment = infoObj.getInt("payment")
+
+                        val infoStr = "이동 시간 : ${minutes}분, 비용 : ${payment}원"
+
                     }
 
                     override fun onError(p0: Int, p1: String?, p2: API?) {
