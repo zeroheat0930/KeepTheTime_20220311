1d7fc225369a4f618bdaad9678883fd5d57e2ad2
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-16 10:02:17 +0900

    약속 등록 성공시, 토스트/ 화면 종료 (임시 처리)

--- app/src/main/java/com/zeroheat/keepthetime_20220311/EditAppointmentActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/EditAppointmentActivity.kt
@@ -112,6 +112,11 @@ class EditAppointmentActivity : BaseActivity() {
                     response: Response<BasicResponse>
                 ) {
 
+                    if(response.isSuccessful){
+                        Toast.makeText(mContext, "약속을 등록했습니다.", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+
                 }
 
                 override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
