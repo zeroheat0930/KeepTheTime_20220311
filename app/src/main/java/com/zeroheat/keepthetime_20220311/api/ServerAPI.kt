b9d721f3d4963e76e27fd96b85899d7ee852787a
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-16 11:13:50 +0900

    파싱하려는 변수 자료형을 Date로 넣으면 어떻게 파싱할건지 그 방법 명시  - retrofit의 세팅 항목 추가

--- app/src/main/java/com/zeroheat/keepthetime_20220311/api/ServerAPI.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/api/ServerAPI.kt
@@ -1,11 +1,13 @@
 package com.zeroheat.keepthetime_20220311.api
 
 import android.content.Context
+import com.google.gson.GsonBuilder
 import com.zeroheat.keepthetime_20220311.utils.ContextUtil
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 class ServerAPI {
 
@@ -54,9 +56,21 @@ class ServerAPI {
                     .addInterceptor(interceptor)
                     .build()
 
+//               Date 자료형으로 파싱 => String을 yyyy-MM-dd HH:mm:ss 으로 파싱해서 저장해야함. (고정된 양식으로 내려줌)
+
+                val gson = GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd HH:mm:ss")  // 서버가 이런 양식으로 보내주는 String을
+                    .registerTypeAdapter(
+                        Date::class.java,
+                        DateDeserializer()
+                    ) // 어떤 형태의 자료형에 적용시킬지?  Date클래스로 파싱.
+                    .create()
+
+
+
                 retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL) // 어느 서버를 기반으로 움직일건지.
-                    .addConverterFactory( GsonConverterFactory.create() ) // gson 라이브러리와 결합
+                    .addConverterFactory( GsonConverterFactory.create( gson ) ) // gson 라이브러리와 결합 + date 파싱 요령 첨부.
                     .client(myClient) // 인터셉터를 부착해둔 클라이언트로 통신하도록
                     .build()
             }
