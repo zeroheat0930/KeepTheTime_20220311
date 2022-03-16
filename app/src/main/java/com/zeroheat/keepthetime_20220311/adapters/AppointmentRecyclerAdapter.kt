19abddaec9bb4fd3174afaa157d095d89da09980
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-16 10:46:53 +0900

    약속 목록 어댑터 추가

---
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/adapters/AppointmentRecyclerAdapter.kt
@@ -0,0 +1,41 @@
+package com.zeroheat.keepthetime_20220311.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.zeroheat.keepthetime_20220311.R
+import com.zeroheat.keepthetime_20220311.datas.AppointmentData
+
+class AppointmentRecyclerAdapter(
+    val mContext: Context,
+    val mList: List<AppointmentData>
+) : RecyclerView.Adapter<AppointmentRecyclerAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(data: AppointmentData) {
+
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val view = LayoutInflater.from(mContext).inflate(R.layout.appointment_list_item, parent, false)
+        return  MyViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val data = mList[position]
+        holder.bind(data)
+
+    }
+
+    override fun getItemCount() = mList.size
+
+}
