1bd0f68176db636ae70bf142596b2a875696c771
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-16 11:02:14 +0900

    어댑터 생성 / 리싸이클러뷰와 연결 / notify 처리

--- app/src/main/java/com/zeroheat/keepthetime_20220311/fragments/AppointmentListFragment.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/fragments/AppointmentListFragment.kt
@@ -7,8 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.zeroheat.keepthetime_20220311.EditAppointmentActivity
 import com.zeroheat.keepthetime_20220311.R
+import com.zeroheat.keepthetime_20220311.adapters.AppointmentRecyclerAdapter
 import com.zeroheat.keepthetime_20220311.databinding.FragmentAppointmentListBinding
 import com.zeroheat.keepthetime_20220311.datas.AppointmentData
 import com.zeroheat.keepthetime_20220311.datas.BasicResponse
@@ -21,6 +23,8 @@ class AppointmentListFragment : BaseFragment() {
     lateinit var binding: FragmentAppointmentListBinding
 
     val mAppointmentList = ArrayList<AppointmentData>()
+
+    lateinit var mAppointmentAdapter: AppointmentRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,6 +55,12 @@ class AppointmentListFragment : BaseFragment() {
     override fun setValues() {
 
         getMyAppointmentListFromServer()
+
+
+        mAppointmentAdapter = AppointmentRecyclerAdapter(mContext, mAppointmentList)
+        binding.appointmentRecyclerView.adapter = mAppointmentAdapter
+        binding.appointmentRecyclerView.layoutManager = LinearLayoutManager(mContext)
+
     }
 
     fun getMyAppointmentListFromServer() {
@@ -62,6 +72,9 @@ class AppointmentListFragment : BaseFragment() {
                     val br = response.body()!!
 
                     mAppointmentList.addAll( br.data.appointments )
+
+                    mAppointmentAdapter.notifyDataSetChanged()
+
 
                 }
 
