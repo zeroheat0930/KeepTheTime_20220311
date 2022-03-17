0733f420464e60c6a6189a76dab74e9bf81a259b
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-17 10:11:25 +0900

    내 장소 목록 조회 API 호출

--- app/src/main/java/com/zeroheat/keepthetime_20220311/ManagePlacesActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/ManagePlacesActivity.kt
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.zeroheat.keepthetime_20220311.databinding.ActivityManagePlacesBinding
+import com.zeroheat.keepthetime_20220311.datas.BasicResponse
+import com.zeroheat.keepthetime_20220311.datas.PlaceData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ManagePlacesActivity : BaseActivity() {
 
     lateinit var binding:ActivityManagePlacesBinding
+
+    val mPlaceList = ArrayList<PlaceData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +29,27 @@ class ManagePlacesActivity : BaseActivity() {
 
     override fun setValues() {
 
+
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        getMyplacesFromServer()
+    }
+
+    fun getMyplacesFromServer(){
+
+        apiList.getRequestUserPlaces().enqueue(object :Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+    }
+
 }
