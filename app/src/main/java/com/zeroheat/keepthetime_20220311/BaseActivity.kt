ffd2f8f39ac06b1aa2781fed93b4aa9b59880169
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-17 10:44:58 +0900

    커스텀 액션바 적용

--- app/src/main/java/com/zeroheat/keepthetime_20220311/BaseActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/BaseActivity.kt
@@ -2,7 +2,9 @@ package com.zeroheat.keepthetime_20220311
 
 import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.zeroheat.keepthetime_20220311.api.APIList
 import com.zeroheat.keepthetime_20220311.api.ServerAPI
 
@@ -20,13 +22,25 @@ abstract class BaseActivity : AppCompatActivity() {
 
         val retrofit = ServerAPI.getRetrofit(mContext)
         apiList = retrofit.create( APIList::class.java )
+
+        supportActionBar?.let {
+            setCustomActionBar()
+        }
     }
 
-    abstract fun setupEvents(
-
-
-    )
+    abstract fun setupEvents()
 
     abstract fun setValues()
 
+    fun setCustomActionBar(){
+
+        val defaultActionBar = supportActionBar!!
+
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+//        defaultActionBar.setDisplayShowCustomEnabled(true) 위랑 같은코드
+        defaultActionBar.setCustomView(R.layout.my_custom_actionbar)
+
+        val toolbar = defaultActionBar.customView.parent as Toolbar
+        toolbar.setContentInsetsAbsolute(0,0)
+    }
 }
