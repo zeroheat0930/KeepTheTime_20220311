bb771901b8affcceee76cd3e4562ed2fea9e42c0
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-17 11:08:27 +0900

    출발 장소 편집 화면의 지도 기능 추가 - 지도 클릭시 마커 이동

--- app/src/main/java/com/zeroheat/keepthetime_20220311/EditMyPlaceActivity.kt
+++ app/src/main/java/com/zeroheat/keepthetime_20220311/EditMyPlaceActivity.kt
@@ -2,10 +2,17 @@ package com.zeroheat.keepthetime_20220311
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.overlay.Marker
 import com.zeroheat.keepthetime_20220311.databinding.ActivityEditMyPlaceBinding
 
 class EditMyPlaceActivity : BaseActivity() {
     lateinit var binding: ActivityEditMyPlaceBinding
+
+//    선택된 장소 저장 변수 / 마커 표시 변수 => 초기값 널 세팅
+    var mSelectedPoint : LatLng? = null
+    var marker : Marker? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,24 @@ class EditMyPlaceActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        binding.naverMapView.getMapAsync {
+
+            val naverMap = it
+
+            naverMap.setOnMapClickListener { pointF, latLng ->
+                if(mSelectedPoint == null){
+//                    처음 지도를 클릭한 상황
+//                    마커를 새로 만들어주자. => 위치 정보는? latLng 변수가 대입될 예정. 새로만들필요 X
+                    marker = Marker()
+                }
+
+                mSelectedPoint = latLng
+
+                marker!!.position = latLng
+                marker!!.map = naverMap
+            }
+        }
 
     }
 }
